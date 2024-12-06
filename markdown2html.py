31e55f09bb6e086a4a5c776290855b820bb2438d
bassem-krayem <bassem.krayem09@gmail.com>
2024-12-06 08:24:17 +0100

    task0 is done

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,8 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
 
     fileName = sys.argv[1]
