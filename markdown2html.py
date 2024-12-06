4684d77f0045e077c6f0582305beb5ad655a454e
bassem-krayem <bassem.krayem09@gmail.com>
2024-12-06 08:23:08 +0100

    task0 is done

--- markdown2html.py
+++ markdown2html.py
@@ -8,14 +8,14 @@ import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     fileName = sys.argv[1]
     outputFileName = sys.argv[2]
 
     if not os.path.isfile(fileName):
-        print("Missing " + fileName)
+        print("Missing " + fileName, file=sys.stderr)
         sys.exit(1)
     else:
         sys.exit(0)
