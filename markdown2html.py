721861bfe31577e2a7def3845ead4c7e6553f209
bassem-krayem <bassem.krayem09@gmail.com>
2024-12-06 10:12:56 +0100

    task1 parsing headings

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,30 @@ this script is to convert the marcdown syntax to html syntax
 import sys
 import os
 
+
+def convertHeadings(line):
+    headingLevel = line.count("#")
+
+    if headingLevel >= 1 and headingLevel <= 6:
+        return (f"<h{headingLevel}>"
+                f"{line[headingLevel:].strip()}</h{headingLevel}>")
+    else:
+        return line  # Return the line unchanged if it's not a heading
+
+
+def mainFunction(fileName, outputFileName):
+    htmlList = []
+
+    with open(fileName, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith("#"):
+                htmlList.append(convertHeadings(line))
+
+    with open(outputFileName, "w") as file:
+        file.write("\n".join(htmlList))
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
@@ -19,4 +43,4 @@ if __name__ == "__main__":
         print("Missing " + fileName, file=sys.stderr)
         sys.exit(1)
     else:
-        sys.exit(0)
+        mainFunction(fileName, outputFileName)
